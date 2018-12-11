705a9fa05676b6d5990c16b79badd2b159379893
Pietro Spadaccino <pietro.spa@gmail.com>
2018-12-11 11:51:40 +0100

    changed net_pretrained

--- pretrainedCNN/net_pretrained.py
+++ pretrainedCNN/net_pretrained.py
@@ -1,66 +1,54 @@
+import sys
+import os
 from keras.applications.inception_v3 import InceptionV3
 from keras.preprocessing import image
 from keras.models import Model
 from keras.layers import Dense, GlobalAveragePooling2D
 from keras import backend as K
-
-# Params
-N_CLASSES = 22
-
-# create the base pre-trained model
-base_model = InceptionV3(weights='imagenet', include_top=False, input_shape=(800,240,3))
-
-# add a global spatial average pooling layer
-x = base_model.output
-x = GlobalAveragePooling2D()(x)
-# let's add a fully-connected layer
-x = Dense(1024, activation='relu')(x)
-# and a logistic layer -- let's say we have 200 classes
-predictions = Dense(N_CLASSES, activation='softmax')(x)
-
-# this is the model we will train
-model = Model(inputs=base_model.input, outputs=predictions)
-
-# first: train only the top layers (which were randomly initialized)
-# i.e. freeze all convolutional InceptionV3 layers
-for layer in base_model.layers:
-    layer.trainable = False
-
-# compile the model (should be done *after* setting layers to non-trainable)
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+sys.path.append(os.path.abspath("utils"))
+import dbloader
+import load_save
 
 
+img_shape = (800,240,3)
+num_classes = 23
+model_filename = "inceptionV3_pretrained.h5"
 
-# TODO load data here 
+model = load_save.load_model(model_filename)
+if not model:
+   # create the base pre-trained model
+   base_model = InceptionV3(weights='imagenet', include_top=False, input_shape=(800,240,3))
+
+   # add a global spatial average pooling layer
+   x = base_model.output
+   x = GlobalAveragePooling2D()(x)
+   # let's add a fully-connected layer
+   x = Dense(1024, activation='relu')(x)
+   # and a logistic layer -- let's say we have 200 classes
+   predictions = Dense(num_classes, activation='softmax')(x)
+
+   # this is the model we will train
+   model = Model(inputs=base_model.input, outputs=predictions)
+
+   # first: train only the top layers (which were randomly initialized)
+   # i.e. freeze all convolutional InceptionV3 layers
+   for layer in base_model.layers:
+      layer.trainable = False
+
+   # compile the model (should be done *after* setting layers to non-trainable)
+   model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+
+model.summary()
 
 
-# train the model on the new data for a few epochs
-model.fit(X, Y, epochs=10, validation_data=(x_val,y_val))
+Xtrain,Ytrain,img_shape,ids_labels,labels_ids = dbloader.load_trainingset(img_shape)
+Xtest,Ytest = dbloader.load_testset(img_shape,labels_ids)
 
 
-'''
-# at this point, the top layers are well trained and we can start fine-tuning
-# convolutional layers from inception V3. We will freeze the bottom N layers
-# and train the remaining top layers.
-
-# let's visualize layer names and layer indices to see how many layers
-# we should freeze:
-for i, layer in enumerate(base_model.layers):
-   print(i, layer.name)
-
-# we chose to train the top 2 inception blocks, i.e. we will freeze
-# the first 249 layers and unfreeze the rest:
-for layer in model.layers[:249]:
-   layer.trainable = False
-for layer in model.layers[249:]:
-   layer.trainable = True
-
-# we need to recompile the model for these modifications to take effect
-# we use SGD with a low learning rate
-from keras.optimizers import SGD
-model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy')
-
-# we train our model again (this time fine-tuning the top 2 inception blocks
-# alongside the top Dense layers
-#model.fit_generator(...)
-'''
+try:
+    model.fit(Xtrain, Ytrain, batch_size=64, epochs=10, verbose=1, validation_data = (Xtest,Ytest), shuffle="batch")
+except KeyboardInterrupt:
+    # Save the model
+    print("Saving...")
+    load_save.save_model(model, model_filename)
+    quit()
