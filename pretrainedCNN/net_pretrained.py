551ffd585be49e651853314a05238555917b757b
Unknown <pietro.spa@gmail.com>
2018-12-11 21:38:06 +0100

    net_pretrained works

--- pretrainedCNN/net_pretrained.py
+++ pretrainedCNN/net_pretrained.py
@@ -11,44 +11,55 @@ import load_save
 
 
 img_shape = (800,240,3)
-num_classes = 23
+num_classes = 24
 model_filename = "inceptionV3_pretrained.h5"
 
 model = load_save.load_model(model_filename)
 if not model:
-   # create the base pre-trained model
-   base_model = InceptionV3(weights='imagenet', include_top=False, input_shape=(800,240,3))
+    print("Creating model...")
 
-   # add a global spatial average pooling layer
-   x = base_model.output
-   x = GlobalAveragePooling2D()(x)
-   # let's add a fully-connected layer
-   x = Dense(1024, activation='relu')(x)
-   # and a logistic layer -- let's say we have 200 classes
-   predictions = Dense(num_classes, activation='softmax')(x)
+    # create the base pre-trained model
+    base_model = InceptionV3(weights='imagenet', include_top=False, input_shape=(800,240,3))
 
-   # this is the model we will train
-   model = Model(inputs=base_model.input, outputs=predictions)
+    # add a global spatial average pooling layer
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    # let's add a fully-connected layer
+    x = Dense(1024, activation='relu')(x)
+    # and a logistic layer -- let's say we have 200 classes
+    predictions = Dense(num_classes, activation='softmax')(x)
 
-   # first: train only the top layers (which were randomly initialized)
-   # i.e. freeze all convolutional InceptionV3 layers
-   for layer in base_model.layers:
+    # this is the model we will train
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    # first: train only the top layers (which were randomly initialized)
+    # i.e. freeze all convolutional InceptionV3 layers
+    for layer in base_model.layers:
       layer.trainable = False
 
-   # compile the model (should be done *after* setting layers to non-trainable)
-   model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+    # compile the model (should be done *after* setting layers to non-trainable)
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
 
 model.summary()
 
 
 Xtrain,Ytrain,img_shape,ids_labels,labels_ids = dbloader.load_trainingset(img_shape)
 Xtest,Ytest = dbloader.load_testset(img_shape,labels_ids)
+#manz = model.evaluate(Xtest, Ytest)
+#print(manz)
+#raise Exception()
 
 
-try:
-    model.fit(Xtrain, Ytrain, batch_size=64, epochs=10, verbose=1, validation_data = (Xtest,Ytest), shuffle="batch")
+try:                             #validation_data = (Xtest,Ytest)
+    model.fit(Xtrain, Ytrain, batch_size=64, epochs=10, verbose=1, shuffle='batch')
 except KeyboardInterrupt:
     # Save the model
     print("Saving...")
     load_save.save_model(model, model_filename)
     quit()
+
+
+# Save the model
+print("Saving...")
+load_save.save_model(model, model_filename)
+quit()
