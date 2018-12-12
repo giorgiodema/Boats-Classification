d53611d4fd7104633af3046c0535f854ffb6a56e
spadala <pietro.spa@gmail.com>
2018-12-12 17:43:19 +0100

    feature extraction

--- pretrainedCNN/extract_features.py
+++ pretrainedCNN/extract_features.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import pickle
+from pdb import set_trace
 from keras.applications.inception_v3 import InceptionV3
 from keras.preprocessing import image
 from keras.models import Model
@@ -16,23 +17,23 @@ num_classes = 24
 
 # create the base pre-trained model
 base_model = InceptionV3(weights='imagenet', include_top=False, input_shape=img_shape)
-'''
-# add a global spatial average pooling layer
-x = base_model.output
-x = GlobalAveragePooling2D()(x)
-# let's add a fully-connected layer
-x = Dense(1024, activation='relu')(x)
-# and a logistic layer -- let's say we have 200 classes
-predictions = Dense(num_classes, activation='softmax')(x)
 
-# this is the model we will train
-model = Model(inputs=base_model.input, outputs=predictions)
-'''
+
 
 Xtrain,Ytrain,img_shape,ids_labels,labels_ids = dbloader.load_trainingset(img_shape)
 
-Xfeat = base_model.predict(Xtrain)
 
-pickle.dump(open('features_pretrained.pickle', 'wb'), Xfeat)
+Xtrain_feat = base_model.predict(Xtrain, verbose=1)
 
+pickle.dump(Xtrain_feat[:2000], open('features_pretrained_0_2000.pickle', 'wb'))
+pickle.dump(Xtrain_feat[2000:], open('features_pretrained_2000_end.pickle', 'wb'))
+pickle.dump(Ytrain[()], open('features_pretrained_Y.pickle', 'wb'))
+del Xtrain_feat
+
+Xtest,Ytest = dbloader.load_testset(img_shape,labels_ids)
+
+Xtest_feat = base_model.predict(Xtest, verbose=1)
+
+pickle.dump(Xtest_feat, open('features_pretrained_test.pickle', 'wb'))
+pickle.dump(Ytest[()], open('features_pretrained_test_Y.pickle', 'wb'))
 
--- pretrainedCNN/net_pretrained.py
+++ pretrainedCNN/net_pretrained.py
@@ -1,9 +1,12 @@
 import sys
 import os
+import pickle
+import numpy as np
+from pdb import set_trace as bp
 from keras.applications.inception_v3 import InceptionV3
 from keras.preprocessing import image
 from keras.models import Model
-from keras.layers import Dense, GlobalAveragePooling2D
+from keras.layers import Dense, GlobalAveragePooling2D, Input
 from keras import backend as K
 sys.path.append(os.path.abspath("utils"))
 import dbloader
@@ -18,11 +21,17 @@ model = load_save.load_model(model_filename)
 if not model:
     print("Creating model...")
 
+    feat_input = Input(shape=(6,23,2048,))  #shape of last inceptionV3 layer
+    x = feat_input
+
+    '''
     # create the base pre-trained model
     base_model = InceptionV3(weights='imagenet', include_top=False, input_shape=(800,240,3))
 
     # add a global spatial average pooling layer
     x = base_model.output
+    bp()
+    '''
     x = GlobalAveragePooling2D()(x)
     # let's add a fully-connected layer
     x = Dense(1024, activation='relu')(x)
@@ -30,28 +39,43 @@ if not model:
     predictions = Dense(num_classes, activation='softmax')(x)
 
     # this is the model we will train
-    model = Model(inputs=base_model.input, outputs=predictions)
+    model = Model(inputs=feat_input, outputs=predictions)
 
     # first: train only the top layers (which were randomly initialized)
     # i.e. freeze all convolutional InceptionV3 layers
-    for layer in base_model.layers:
-      layer.trainable = False
-
+    #for layer in base_model.layers:
+    #  layer.trainable = False
+    
     # compile the model (should be done *after* setting layers to non-trainable)
-    model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
 
 model.summary()
 
 
-Xtrain,Ytrain,img_shape,ids_labels,labels_ids = dbloader.load_trainingset(img_shape)
-Xtest,Ytest = dbloader.load_testset(img_shape,labels_ids)
+#Xtrain,Ytrain,img_shape,ids_labels,labels_ids = dbloader.load_trainingset(img_shape)
+#Xtest,Ytest = dbloader.load_testset(img_shape,labels_ids)
 #manz = model.evaluate(Xtest, Ytest)
 #print(manz)
 #raise Exception()
 
+# Training set
+Xtrain1 = pickle.load(open("features_pretrained_0_2000.pickle", "rb"))
+Xtrain2 = pickle.load(open("features_pretrained_2000_end.pickle", "rb"))
+Xtrain = np.concatenate((Xtrain1, Xtrain2))
+del Xtrain1
+del Xtrain2
+Ytrain = pickle.load(open("features_pretrained_Y.pickle", "rb"))
+
+# Test set
+Xtest = pickle.load(open("features_pretrained_test.pickle", "rb"))
+Ytest = pickle.load(open("features_pretrained_test_Y.pickle", "rb"))
+
+
+#bp()
 
 try:                             #validation_data = (Xtest,Ytest)
-    model.fit(Xtrain, Ytrain, batch_size=64, epochs=10, verbose=1, shuffle='batch')
+    model.fit(Xtrain, Ytrain, batch_size=64, epochs=10, verbose=1, validation_split=0.2, shuffle=True)
 except KeyboardInterrupt:
     # Save the model
     print("Saving...")
