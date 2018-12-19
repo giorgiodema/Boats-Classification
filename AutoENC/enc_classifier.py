448bdc91e4e824049e24e63ca1e788b501101faa
giorgiodema <33451037+giorgiodema@users.noreply.github.com>
2018-12-19 13:15:23 +0100

    Encoder e CNN

--- AutoENC/enc_classifier.py
+++ AutoENC/enc_classifier.py
@@ -3,7 +3,7 @@ import os
 from os.path import join
 import pickle
 from keras.preprocessing import image
-from keras.layers import Input,Conv2D,MaxPooling2D,UpSampling2D,Flatten,Dense, Reshape
+from keras.layers import Input,Conv2D,MaxPooling2D,UpSampling2D,Flatten,Dense, Reshape,Dropout
 from keras.models import Model
 from keras.backend import reshape,flatten
 from keras.callbacks import CSVLogger, TensorBoard, ModelCheckpoint
@@ -42,27 +42,49 @@ class AutoEncSVMclassifier:
             self.clf = sklearn.svm.LinearSVC(max_iter=100000)
             return
         print("Creating clf...")
-        input_img = Input(shape=img_shape, name='input')  # adapt this if using `channels_first` image data format
-        x = Conv2D(8, (3, 3), activation='relu', padding='same')(input_img)
-        x = MaxPooling2D((2, 2), padding='same')(x)
-        x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
-        x = MaxPooling2D((10, 10), padding='same')(x)
-        x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-        x = MaxPooling2D((2, 2), padding='same')(x)
-        x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
-        encoded = Flatten(name='encoded')(x)
+
+        # The input layer is a matrix with shape(240,800,3)
+        input_img = Input(shape=img_shape, name='input')
+        # 6 filters (5,5)   --> shape = (240,800); parameters = 100
+        x = Conv2D(filters=6, kernel_size=(5,5),padding='same',activation='relu')(input_img)
+        # MaxPooling (2,2)  --> shape = (120,400,3)
+        x = MaxPooling2D(pool_size=(2,2))(x)
+        # 9 filters (5,5)   --> shape = (120,400,9); parameters = (5*5*8)*4 = 800
+        x = Conv2D(filters=9,kernel_size=(5,5),padding='same',activation='relu')(x)
+        # MaxPooling (2,2)--> shape = (60,200,9)
+        x = MaxPooling2D(pool_size=(2,2))(x)
+        # 12 filters (5,5)   --> shape = (60,200,12); parameters = (5*5*8)*4 = 800
+        x = Conv2D(filters=12,kernel_size=(5,5),padding='same',activation='relu')(x)
+        # MaxPooling (5,5)--> shape = (12,40,12)
+        x = MaxPooling2D(pool_size=(5,5))(x)
+
+        # Flatten  --> output shape = (12*40*12) = 5760
+        x = Flatten()(x)
+        # Dropout
+        x = Dropout(rate=0.4,seed=None)(x)
+        # FC layer --> parameters = 4320*2160 = 1'440'000
+        encoded = Dense(units=2880,activation='relu',name='encoded')(x)
+ 
+        x = Dropout(rate=0.4,seed=None)(encoded)
+        # shape = 4320
+        x = Dense(units=5760,activation='relu')(x)
+        # shape = (12,40,9)
+        x = Reshape((12,40,12))(x)
+        # shape = (12,40,9)
+        x = Conv2D(filters=9,kernel_size=(5,5),padding='same',activation='relu')(x)
+        # shape = (60,200,9)
+        x = UpSampling2D(size=(5,5))(x)
+        # shape = (60,200,9)
+        x = Conv2D(filters=9,kernel_size=(5,5),padding='same',activation='relu')(x)
+        # shape = (120,400,9)
+        x = UpSampling2D(size=(2,2))(x)
+        # shape = (120,400,3)
+        x = Conv2D(filters=3,kernel_size=(5,5),padding='same',activation='relu')(x)
+        # shape = (240,800,3)
+        decoded = UpSampling2D(size=(2,2))(x)
 
 
-        # at this point the representation is (4, 4, 8) i.e. 128-dimensional
-        x = Reshape((6,20,64))(encoded)
-        x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
-        x = UpSampling2D((10, 10))(x)
-        x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-        x = UpSampling2D((2, 2))(x)
-        x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
-        x = UpSampling2D((2, 2))(x)
-        x = Conv2D(8, (3, 3), activation='relu',padding='same')(x)
-        decoded = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(x)
+
 
         self.autoencoder = Model(input_img, decoded)
         self.autoencoder.compile(optimizer='adam', loss='categorical_crossentropy',metrics=['accuracy'])
--- CNN/cnnmodel.py
+++ CNN/cnnmodel.py
@@ -32,20 +32,20 @@ class ConvolutionalNN:
         # MaxPooling (2,2)--> shape = (60,200,9)
         p2 = MaxPool2D(pool_size=(2,2))(f2)
         # 12 filters (5,5)   --> shape = (60,200,12); parameters = (5*5*8)*4 = 800
-        f3 = Conv2D(filters=9,kernel_size=(5,5),padding='same',activation='relu')(p2)
-        # MaxPooling (5,5)--> shape = (12,40,9)
+        f3 = Conv2D(filters=12,kernel_size=(5,5),padding='same',activation='relu')(p2)
+        # MaxPooling (5,5)--> shape = (12,40,12)
         p3 = MaxPool2D(pool_size=(5,5))(f3)
 
-        # Flatten  --> output shape = (12*40*9) = 4320
+        # Flatten  --> output shape = (12*40*12) = 5760
         fl = Flatten()(p3)
         # Dropout
         dr1 = Dropout(rate=0.4,seed=None)(fl)
-        # FC layer --> parameters = 4320*864 = 1'440'000
-        d1 = Dense(units=864,activation='relu')(dr1)
+        # FC layer --> parameters = 5760*1152 = 1'440'000
+        d1 = Dense(units=1152,activation='relu')(dr1)
         #Dropout
         dr2 = Dropout(rate=0.4,seed=None)(d1)
-        # FC layer --> parameters = 864*432 = 60'000
-        d2 = Dense(units=432,activation='relu')(dr2)
+        # FC layer --> parameters = 1152*576 = 60'000
+        d2 = Dense(units=576,activation='relu')(dr2)
         output = Dense(units=num_cat,activation='softmax')(d2)
         # TOTAL TRAINABLE PARAMETERS --> 1'503'780
 
