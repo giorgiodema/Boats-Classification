d74f0ffcfba5b6546f03f19a09497e3995e63cc4
giorgiodema <giorgiodema@gmail.com>
2018-12-22 00:37:49 +0100

    svm ref

--- SVM/classifier.py
+++ SVM/classifier.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from os.path import join
 import pickle
 from pdb import set_trace
 from keras.applications.inception_v3 import InceptionV3
@@ -11,26 +12,22 @@ import sklearn.svm
 import numpy as np
 sys.path.append(os.path.abspath("utils"))
 import dbloader
+from load_save import load_model,save_model
 
-
-def save_classifier(path,clf):
-    with open(path,"wb") as f:
-        pickle.dump(clf,f)
-
-def load_classifier(path):
-    clf = None
-    if os.path.exists(path):
-        with open(path,"rb") as f:
-            clf = pickle.load(f)
-    return clf
+model_path = join("raw","svmCNN.h5")
+tensorboardpath = join('tmp','svmCNN')
 
 class SVMwithFeatures:
 
     def __init__(self,img_shape):
-        self.model = InceptionV3(weights='imagenet', include_top=False, input_shape=img_shape)
-        x1 = MaxPooling2D(pool_size=(2,2), strides=2)(self.model.layers[-1].output)
-        x2 = Flatten()(x1)        
-        self.model = Model(self.model.input,x2)
+        self.model = load_model(model_path)
+        if not self.model:
+            print("Creating model")
+            self.trained=False
+            self.model = InceptionV3(weights='imagenet', include_top=False, input_shape=img_shape)
+            x1 = MaxPooling2D(pool_size=(2,2), strides=2)(self.model.layers[-1].output)
+            x2 = Flatten()(x1)        
+            self.model = Model(self.model.input,x2)
 
         self.clf = sklearn.svm.LinearSVC(max_iter=100000)
 
